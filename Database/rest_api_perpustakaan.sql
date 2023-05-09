112af8942cdc3461c5255cd091597cabfc345aa1
deliyasyafa290304 <deliyasyafa@gmail.com>
2023-05-09 17:38:03 +0700

    #1

---
+++ Database/rest_api_perpustakaan.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 09, 2023 at 11:52 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rest_api_perpustakaan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `kode_buku` varchar(20) NOT NULL,
+  `judul_buku` varchar(50) NOT NULL,
+  `pengarang` varchar(50) NOT NULL,
+  `penerbit` varchar(50) NOT NULL,
+  `Tahun` varchar(50) NOT NULL,
+  `jumlah_buku` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`kode_buku`, `judul_buku`, `pengarang`, `penerbit`, `Tahun`, `jumlah_buku`) VALUES
+('B0001', 'Malin kundang', 'Ina Asiah', 'Gramedia', '2018', '100');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`kode_buku`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Project Vb/Perpustakaan_restApi/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Web.Script.Serialization
+Imports Newtonsoft.Json
+Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim url As String = "http://localhost:8080/buku"
+        j()
+        Dim request As HttpWebRequest = CType(WebRequest.Create(url), HttpWebRequest)
+        request.Method = "GET"
+
+        Dim response As HttpWebResponse = request.GetResponse()
+        Dim reader As New StreamReader(response.GetResponseStream())
+        Dim responseText As String = reader.ReadToEnd()
+
+        ' Konversi respons JSON ke dalam DataTable
+        Dim table As DataTable = JsonConvert.DeserializeObject(Of DataTable)(responseText)
+
+        ' Tampilkan data pada DataGridView
+        DataGridView1.DataSource = table
+
+    End Sub
+    Private Sub simpanapi()
+        
+    End Sub
+End Class
